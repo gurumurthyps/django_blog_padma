438650377b6ac23f0f07130c14a4d32dbf277f3b
guru-sellinall <gurumurthy@graas.ai>
2023-08-26 18:31:47 +0530

    login page and sign up

--- core/helpers.py
+++ core/helpers.py
@@ -2,13 +2,28 @@ from django.core.mail import send_mail
 
 import uuid
 from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
-def send_forgot_password_mail(email,token):
 
+import socket
+
+def send_forgot_password_mail(email,token,protocol,socket,port):
+
+
+    # try:
+    #     HOSTNAME = socket.gethostname()
+    # except:
+    #     HOSTNAME = 'localhost'
+    url=f'{protocol}://{socket}:{port}/change_password/{token}/'
     subject='Your Forgot Password Link'
-    message=f'Click on the link'
+    message_html = render_to_string('change_password_template.html', {'url': url})
+
+    # Create a plain text version of the email (optional)
+    message_text = strip_tags(message_html)
     email_from=settings.EMAIL_HOST_USER
     recepient_list=[email]
-    kindle=send_mail(subject,message,email_from, recepient_list)
+    kindle=send_mail(subject,message_text,email_from, recepient_list,html_message=message_html)
     print(kindle)
     return True
--- core/views.py
+++ core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render,redirect
 from rest_framework.views import APIView
-from .models import *
+from core.models import Profile
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login, logout
 from .serializer import *
 from django.contrib.auth.models import User
 import uuid
 from .helpers import send_forgot_password_mail
+from django.urls import reverse
+
 
 
 # Create your views here.
@@ -42,14 +44,17 @@ def main_dashboard(request):
         return render(request, 'index.html', context)
 
 
-def login(request):
+def login_base(request):
+    res = logout(request)
     return render(request,'base_site.html')
 
 def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
+        print(username,password)
         user = authenticate(username=username, password=password)
+        print(user, "user")
         if user is not None:
             login(request, user)
             return redirect('/')
@@ -58,6 +63,31 @@ def login_user(request):
 
     return render(request, 'index.html')
 
+def change_password(request,token):
+    context={}
+    # try:
+    profile_obj=Profile.objects.filter(forget_password_token=token).first()
+    print(profile_obj)
+    print(profile_obj.user.id)
+    if request.method=='POST':
+        new_password=request.POST.get("password1")
+        new_password2 = request.POST.get("password2")
+        user_id= request.POST.get("user_id")
+        if user_id is None:
+            request.session['message'] = 'Invalid UserID'
+            return redirect(f'/change_password/{token}')
+        if new_password!=new_password2:
+            request.session['message'] = 'Both Password should be equal'
+            return redirect(f'/change_password/{token}')
+        user_obj=User.objects.get(id=user_id)
+        print(user_obj)
+        user_obj.set_password(new_password)
+        user_obj.save()
+        return redirect('/login')
+    context={'user_id':profile_obj.user.id}
+    # except Exception as e:
+    #     print(e)
+    return render(request,'change_password.html',context)
 
 
 def logout_user(request):
@@ -66,15 +96,33 @@ def logout_user(request):
 	return response
 
 def forget_password(request):
-    try:
-        if request.method=='POST':
-            username=request.POST.get("username")
-            user_obj=User.objects.filter(username=username).first()
-            if user_obj:
-                token=str(uuid.uuid4())
-                send_forgot_password_mail(user_obj,token)
+    context={'success_message':''}
+    # try:
+    if request.method=='POST':
+        username=request.POST.get("username")
+        user_obj=User.objects.filter(username=username).first()
+        print(user_obj,"user_obj")
+        if user_obj:
+            token=str(uuid.uuid4())
+            profile_obj, created=Profile.objects.get_or_create(user=user_obj)
+            print(profile_obj,"profile_obj")
+            profile_obj.forget_password_token=token
+            profile_obj.save()
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]
             else:
-                return render(request,'forgot_password.html', {'error_message':'Invalid Username'})
-    except Exception as e:
-        print(e)
-    return render(request,'forgot_password.html')
+                ip = request.META.get('REMOTE_ADDR')
+            port=request.META.get('SERVER_PORT', '')
+            print(ip,"ip")
+            if request.is_secure():
+                protocol = 'https'
+            else:
+                protocol = 'http'
+            send_forgot_password_mail(user_obj,token,protocol,ip,port)
+            context["success_message"]="An Email link is sent to the registered email for verification, please change the password through the link "
+        else:
+            return render(request,'forgot_password.html', {'error_message':'Invalid Username'})
+    # except Exception as e:
+    #     print(e)
+    return render(request,'forgot_password.html',context)
